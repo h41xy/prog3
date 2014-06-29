b4608546c9d24a38185bd421110bda1c1520e896
h41xy <urs.oberdorf@autistici.org>
2014-06-29 20:22:51 +0200

    ActionListener auf die buttons gelegt und versucht ein textfeld zu implementieren

--- prototyp/ProtGridFrame.java
+++ prototyp/ProtGridFrame.java
@@ -1,30 +1,54 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+import javax.swing.JTextField;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ProtGridFrame extends JFrame {
 
-  JPanel contentPane;
+  Container contentPane;
+  JTextField searchField;
+  JPanel buttonGrid;
 
   private ProtGridFrame() {
 
     setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-    contentPane = new JPanel(new ProtGridLayout());
+    contentPane = getContentPane();
 
-    contentPane.add(new ProtGridButton("Geraet 1"));
-    contentPane.add(new ProtGridButton("Geraet 2"));
-    contentPane.add(new ProtGridButton("Geraet 3"));
-    contentPane.add(new ProtGridButton("Geraet 4"));
-    contentPane.add(new ProtGridButton("Geraet 5"));
-    contentPane.add(new ProtGridButton("Geraet 6"));
+    searchField = new JTextField("Suchen");
+    contentPane.add(searchField);
 
-    setContentPane(contentPane);
+    buttonGrid = new JPanel(new ProtGridLayout());
+
+    // Folgender Teil ist fuer Jeden Button zu erstellen ---
+    ProtGridButton buttonGeraet01 = new ProtGridButton("Geraet 1");
+    buttonGeraet01.addActionListener(new ActionButtonGeraet01());
+    buttonGrid.add(buttonGeraet01);
+    // bis hier --------------------------------------------
+    // Es muessen (glaube ich) jeweils unterschiedliche ActionListener
+    // fuer jeden Button implementiert werden. Vielleicht gehts aber auch
+    // mit einer Abfrage welcher Button die Action ausgeloest hat.
+    
+    buttonGrid.add(new ProtGridButton("Geraet 2"));
+    buttonGrid.add(new ProtGridButton("Geraet 3"));
+    buttonGrid.add(new ProtGridButton("Geraet 4"));
+    buttonGrid.add(new ProtGridButton("Geraet 5"));
+    buttonGrid.add(new ProtGridButton("Geraet 6"));
+
+    contentPane.add(buttonGrid);
     pack();
     setVisible(true);
   }
 
+  class ActionButtonGeraet01 implements ActionListener {
+    public void actionPerformed (ActionEvent e) {
+      System.out.println("Geraet01Frame");
+      // Hier kommt Logik fuer die Darstellung des naechsten Frames
+    }
+  }
   public static void main(String[] args) {
     ProtGridFrame prot = new ProtGridFrame();
   }
